5f37df2922f6c16ff5743f2d7fe3e75d05f8b068
moraleseconomia <morales.economia@gmail.com>
2020-09-17 18:26:28 -0600

    fgfd
    fgfd

---
+++ probando_vscode.py
@@ -0,0 +1,15 @@
+
+# %%
+5 + 6
+# %%
+print(56+98)
+# %%
+print(8987+6545)
+
+# %%
+import matplotlib.pyplot as plt
+x = [5,8,5,7,8,9,8,7,5,9]
+y = [7,5,3,7,3,4,5,8,6,3]
+plt.plot(x, y)
+plt.show()
+# %%
